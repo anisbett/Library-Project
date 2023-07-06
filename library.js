3b9d5a51cd6b7b1ace8db3b9e29ec62ff4a72254
anisbett <ajaninisbett@outlook.com>
2023-07-05 21:21:17 -0400

    Stuff added to javascript file

--- library.js
+++ library.js
@@ -1,2 +1,12 @@
 // Make a plan//
 //Break tasks down into small parts//
+
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
