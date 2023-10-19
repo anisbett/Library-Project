92a345d68f3a527608784cb26c183f21cc24c5c4
anisbett <ajaninisbett@outlook.com>
2023-10-19 11:48:05 -0400

    Work done

--- library.js
+++ library.js
@@ -1,38 +1,24 @@
-// Make a plan//
-//Break tasks down into small parts//
-
 let myLibrary = [];
 
 function Book(author, title, pages, read) {
   // the constructor...
   this.author = author
-  this.tilte = title
+  this.title = title
   this.pages = pages
   this.read = read
 }
 
-function addBookToLibrary() {
-  // do stuff here
-  let author = prompt("Enter the author of the book");
-  let title = prompt("Enter the title of the book");
-  let pages = prompt("Enter the number of pages in the book");
-  let answer = prompt("Type true if you read the book or type false if you have not read the book");
-  let test = answer.toLowerCase();
-  let read;
-  if (test === "true"){
-    read = true;
-  }
-  else if (test === "false"){
-    read = false;
-  }
-
-  const book = new Book(author, title, pages, read);
-  myLibrary.push(book);
-
-
-}
+Book.prototype.toggleReadStatus = function() {
+  this.read =! this.read; // Toggle between true and false
+};
 
 function displayBooks(arr) {
+
+  var booksContainer = document.querySelector('#books-container');
+
+  // Clear existing content
+  booksContainer.innerHTML = '';
+
   let i = 0;
   let length = arr.length;
   for(i=0; i < length; i++){
@@ -40,15 +26,41 @@ function displayBooks(arr) {
     card.innerHTML = `<h5>${arr[i].title}</h5>
     <br>${arr[i].author}
     <br>${arr[i].pages}
-    <br>${arr[i].read}`
+    <br>${arr[i].read}
+    <br><button class="remove-button" data-index="${i}">Remove Book</button>
+    <br><button class="toggle-read-button" data-index="${i}">Toggle Read Status</button>
+    `;
+
+    booksContainer.appendChild(card);
 
   }
 }
 
-function addBook(){
-  let author = document.querySelector('#author');
-  let title = document.querySelector('#title');
-  let pages = document.querySelector('#pages');
+document.getElementById('books-container').addEventListener('click', function (event) {
+  if (event.target.classList.contains('remove-button')) {
+    // Get the index from the data-index attribute
+    var index = event.target.getAttribute('data-index');
+
+    // Remove the book from the library array
+    myLibrary.splice(index, 1);
+
+    // Render the updated list of books
+    displayBooks(myLibrary);
+  } else if (event.target.classList.contains('toggle-read-button')) {
+    // Toggle the read status of the book
+    var index = event.target.getAttribute('data-index');
+    myLibrary[index].toggleReadStatus();
+    //Render the updated list of books
+    displayBooks(myLibrary);
+  }
+});
+
+function addBook(event){
+  event.preventDefault();
+
+  let author = document.querySelector('#author').value;
+  let title = document.querySelector('#title').value;
+  let pages = document.querySelector('#pages').value;
   
   let radios = document.getElementsByName('answer');
   let selectedValue = '';
@@ -88,8 +100,7 @@ function hideForm() {
 
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#book-form').style.display = 'none';
-  document.querySelector('#add-book').addEventListener('click', ()=> loadForm);
-  document.querySelector('#submit-form').addEventListener('click', ()=> hideForm);
+  document.querySelector('#add-book').addEventListener('click', loadForm);
+  document.querySelector('#submit-form').addEventListener('click', hideForm);
 });
 
-//event.preventDefault() needs to be added
